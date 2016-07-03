9ed1286404509a6ff7e15e13a0ebf2bb610f3ddd
蔡健 <jian.cai@qunar.com>
2016-07-03 10:47:56 +0800

    add ICacheTemplate

---
+++ src/main/java/com/jianchen/cache/CacheException.java
@@ -0,0 +1,7 @@
+package com.jianchen.cache;
+
+//--------------------- Change Logs----------------------
+// <p>@author jian.cai Initial Created at 2016-07-03</p>
+//-------------------------------------------------------
+public class CacheException extends RuntimeException {
+}
---
+++ src/main/java/com/jianchen/cache/ICacheTemplate.java
@@ -0,0 +1,106 @@
+package com.jianchen.cache;
+
+//--------------------- Change Logs----------------------
+// <p>@author jian.cai Initial Created at 2016-07-03</p>
+//-------------------------------------------------------
+public interface ICacheTemplate<CacheClient> {
+
+    /**
+     * 获取底层真正的缓存实现
+     *
+     * @return
+     */
+    public CacheClient getClient();
+
+    /**
+     * 设置缓存值,使用默认的过期时间
+     *
+     * @param key
+     * @param value
+     * @param <T>
+     * @return
+     * @throws CacheException
+     */
+    public <T> boolean set(String key, T value) throws CacheException;
+
+    /**
+     * 获取缓存值
+     *
+     * @param key
+     * @param <T>
+     * @return
+     * @throws CacheException
+     */
+    public <T> T get(String key) throws CacheException;
+
+    /**
+     * 设置缓存值,使用指定的过期时间
+     *
+     * @param key
+     * @param value
+     * @param <T>
+     * @return
+     * @throws CacheException
+     */
+    public <T> boolean set(String key, T value, int expireTime) throws CacheException;
+
+    /**
+     * set的无异常版本
+     *
+     * @param key
+     * @param value
+     * @param <T>
+     * @return
+     */
+    public <T> boolean setWithoutException(String key, T value);
+
+    /**
+     * get的无异常版本
+     *
+     * @param key
+     * @param <T>
+     * @return
+     */
+    public <T> T getWithoutException(String key);
+
+    /**
+     * 获取缓存值,取不到默认返回null
+     *
+     * @param key
+     * @param <T>
+     * @return
+     * @throws CacheException
+     */
+    public <T> T getWithDefaultNull(String key);
+
+    /**
+     * 获取缓存值,取不到或者抛出异常返回默认值
+     *
+     * @param key
+     * @param <T>
+     * @return
+     * @throws CacheException
+     */
+    public <T> T getWithDefault(String key, T defaultValue);
+
+
+    /**
+     * 删除指定key
+     *
+     * @param key
+     * @return
+     * @throws CacheException
+     */
+    public boolean delete(String key) throws CacheException;
+
+    /**
+     * 删除指定key,无异常版本
+     *
+     * @param key
+     * @return
+     * @throws CacheException
+     */
+    public boolean deleteWithoutException(String key);
+
+
+}
